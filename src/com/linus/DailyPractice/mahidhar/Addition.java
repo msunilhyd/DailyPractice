adf6bed821e23f65062ecff0fb6a54562268de53
illuri96 <reddymahidhar9@gmail.com>
2018-10-14 11:00:19 -0400

    14-oct-18 : Added Addition Program

---
+++ src/com/linus/DailyPractice/mahidhar/Addition.java
@@ -0,0 +1,13 @@
+package com.linus.DailyPractice.mahidhar;
+
+public class Addition 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Added Value is : "+add(5,10));
+	}
+	public static int add(int a, int b)
+	{
+		return a+b;
+	}
+}
